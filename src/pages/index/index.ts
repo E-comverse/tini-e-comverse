f606b2b178277547ec855fd3dbdd1558e39828db
Man Nguyen <nnman.vn@gmail.com>
2022-07-09 12:43:20 +0700

    create order

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -2,7 +2,13 @@
 /// <reference lib="dom" /> 
 // => fix missing setTimeout
 
-import { AUTH_TOKEN_API, CLIENT_ID, EXCHANGE_AUTH_CODE_API, CREATE_SIGNATURE_API } from '../../constant';
+import {
+  AUTH_TOKEN_API,
+  CLIENT_ID,
+  EXCHANGE_AUTH_CODE_API,
+  CREATE_SIGNATURE_API,
+  CREATE_ORDER_API
+} from '../../constant';
 import { _post_ } from '../../services/auth.service';
 
 interface HomeData {
@@ -13,9 +19,33 @@ interface HomeDataCustomMethods {
   onTap: () => void;
 }
 
+interface Item {
+  name: string;
+  quantity: number;
+  price: number;
+}
+
 const exchangeAuthCode = async (code: string) => {
   return _post_(EXCHANGE_AUTH_CODE_API, {
     code,
+  }).then(res => {
+    return res;
+  })
+}
+
+const createOrder = async (code: string) => {
+  // const customer_id = customer.id;
+  // const items: Item[] = [
+  //   {
+  //     name: 'Man Ne',
+  //     quantity: 4,
+  //     price: 100
+  //   }
+  // ];
+  // const customer_info = customer;
+
+  return _post_(CREATE_ORDER_API, {
+    code
   }).then(res => {
     return res;
   })
@@ -41,8 +71,11 @@ Page<HomeData, HomeDataCustomMethods>({
       success: async ({ authCode, authSuccessScope, authErrorScope }) => {
         console.log(authCode, authSuccessScope, authErrorScope);
         // console.log(signature);
-        const { data, error } = await exchangeAuthCode(authCode);
-        console.log(data, error);
+        // const { data, error } = await exchangeAuthCode(authCode);
+        // const { access_token: accessToken, customer } = data;
+        const order = await createOrder(authCode);
+
+        console.log('order', order);
       },
       // scopes: ['offline']
       // fail: (res) => {
