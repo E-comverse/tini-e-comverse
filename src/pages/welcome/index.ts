3533ef0620463df2144289168c8dd552571808db
Hoang Tien Thinh <85208341+TienThinh-fe@users.noreply.github.com>
2022-07-10 02:48:54 +0700

    screen: add welcome screen (#1)

---
+++ src/pages/welcome/index.ts
@@ -0,0 +1,11 @@
+Page({
+  toTiniWorld() {
+    my.navigateTo({ url: 'pages/tini-world/index' });
+  },
+  toCheckout() {
+    my.navigateTo({ url: "pages/checkout/index" });
+  },
+  toTracking() {
+    my.navigateTo({ url: "pages/tracking-shipping/index" });
+  }
+});
